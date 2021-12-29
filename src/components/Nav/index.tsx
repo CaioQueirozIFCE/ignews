9b8df6e898db79b932187a41bb5d2b3ecea73a55
Caio Queiroz <caio.softbuilder>
2021-12-28 23:53:13 -0300

    aplicando estilização dinamica a mudanã de pagina

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import styles from './styles.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Nav: React.FC = () => {
+    const {pathname} = useRouter();
 
     return(
         <nav className={styles.navContainer}>
-            <a className={styles.active} href="#">Home</a>
-            <a href="#">Posts</a>
+            <Link  href="/" passHref>
+                <div className={pathname === '/' ? styles.active : ''}>
+                    Home
+                </div>
+            </Link>
+            <Link href="/posts" passHref>
+                <div className={pathname === '/posts' ? styles.active : ''}>
+                    Posts
+                </div>
+            </Link>
         </nav>
     );
 }
