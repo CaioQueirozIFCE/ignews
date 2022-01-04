d8b22ae76e6c5e418c1da9910a312c71075710e2
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-01-04 12:27:49 -0300

    add consoles para debugg actived link

--- src/components/ActivedLink/index.tsx
+++ src/components/ActivedLink/index.tsx
@@ -16,9 +16,12 @@ const ActivedLink = ({activedClassName, children, subPages, ...rest}: ActivedLin
             return asPath.match("" + rest.href.toString() + "(\/(.){0,})?") !== null ? activedClassName : '';
         }
 
+        console.log('activedLink -> asPath => ', asPath)
+        console.log('activedLink -> rest.href => ', rest.href)
+        console.log('activedLink -> verificação => ', asPath === rest.href)
         return asPath === rest.href ? activedClassName : '';
     }, [asPath, activedClassName, rest.href, subPages]);
-    console.log('activedLink -> asPath => ', asPath)
+    
     console.log('activedLink -> className => ', className)
     return(
         <Link {...rest}>    
