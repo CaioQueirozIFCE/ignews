814b7f45c154d3a3d2776e56937b6985c5b58124
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-01-04 13:03:18 -0300

    retirada de consoles de debuggs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import {useWindowResize} from '../../hooks/useWindowResize';
 
 const Header: React.FC = () => {
     const {width} = useWindowResize() || {width: 1000};
-    console.log('header')
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
